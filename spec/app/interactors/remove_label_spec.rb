8defedcaf096469987df1862a4f55fa69f46b26a
Pablo Cantero <pablohstc@gmail.com>
2018-04-14 19:26:28 -0400

    RSpec add & remove labels

---
+++ spec/app/interactors/remove_label_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+RSpec.describe RemoveLabel do
+  let(:number) { 'number' }
+  let(:repo_full_name) { 'octocat/Hello-World' }
+  let(:access_token) { 'token' }
+  let(:client) { double 'Octokit' }
+  let(:label) { Constants::IN_PROGRESS }
+
+  before do
+    allow_any_instance_of(described_class).to receive(:client).and_return(client)
+  end
+
+  describe '#call' do
+    specify do
+      expect(client).to receive(:remove_label).with(repo_full_name, number, [label])
+
+      described_class.call!(
+        number: number,
+        repo_full_name: repo_full_name,
+        access_token: access_token,
+        label: label
+      )
+    end
+  end
+end
--- spec/app/interactors/webhooks/handle_issues_action_closed_spec.rb
+++ spec/app/interactors/webhooks/handle_issues_action_closed_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Webhooks
   RSpec.describe HandleIssuesActionClosed do
     let(:issue) { File.read('./spec/fixtures/github/issue.json') }
-    let(:id) { issue['number'] }
+    let(:number) { issue['number'] }
     let(:repo_full_name) { 'octocat/Hello-World' }
     let(:payload) { { 'issue' => issue } }
     let(:access_token) { 'token' }
@@ -11,7 +11,7 @@ module Webhooks
     describe '#call' do
       specify do
         expect(RemoveLabel).to receive(:call!).with(
-          number: id,
+          number: number,
           label: [Constants::IN_PROGRESS, Constants::READY_FOR_REVIEW, Constants::REVIEW_REQUESTED],
           repo_full_name: repo_full_name,
           access_token: access_token
