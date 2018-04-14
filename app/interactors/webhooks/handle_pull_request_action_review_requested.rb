155f4e5fe55511c080b60da3a87a0bbc8be3f948
Pablo Cantero <pablohstc@gmail.com>
2018-04-14 18:31:02 -0400

    RSpec pull request review requested

--- app/interactors/webhooks/handle_pull_request_action_review_requested.rb
+++ app/interactors/webhooks/handle_pull_request_action_review_requested.rb
@@ -3,11 +3,16 @@ module Webhooks
     include Interactor
     include InteractorHelpers
 
-    def_delegators :context, :payload
+    def_delegators :context, :payload, :repo_full_name, :access_token
 
     def call
-      Webhooks::FindFixableIssues.call!(payload['pull_request']['body']).ids.each do |id|
-        AddLabelToAnIssue.call!(repo_full_name: repo_full_name, id: id, label: Constants::REVIEW_REQUESTED)
+      Webhooks::FindFixableIssues.call!(body: payload['pull_request']['body']).ids.each do |id|
+        AddLabelToAnIssue.call!(
+          repo_full_name: repo_full_name,
+          id: id,
+          label: Constants::REVIEW_REQUESTED,
+          access_token: access_token
+        )
       end
     end
   end
