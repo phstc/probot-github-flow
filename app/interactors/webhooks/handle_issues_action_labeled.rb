2979c6db028d6f13414e3853447937dfc976622f
Pablo Cantero <pablohstc@gmail.com>
2018-04-14 15:48:57 -0400

    :lipstick:

--- app/interactors/webhooks/handle_issues_action_labeled.rb
+++ app/interactors/webhooks/handle_issues_action_labeled.rb
@@ -10,33 +10,33 @@ module Webhooks
 
       case payload.dig('label', 'name')
       when Constants::READY_FOR_REVIEW
-        RemoveLabel.call!(
-          repo_full_name: repo_full_name,
-          id: id,
-          label: Constants::IN_PROGRESS,
-          access_token: access_token
-        )
+        remove_label(id, Constants::IN_PROGRESS)
       when Constants::REJECTED
-        RemoveLabel.call!(
-          repo_full_name: repo_full_name,
-          id: id,
-          label: [Constants::IN_PROGRESS, Constants::READY_FOR_REVIEW],
-          access_token: access_token
-        )
+        remove_label(id, [Constants::IN_PROGRESS, Constants::READY_FOR_REVIEW])
       when Constants::REVIEW_REQUESTED
-        RemoveLabel.call!(
-          repo_full_name: repo_full_name,
-          id: id,
-          label: Constants::IN_PROGRESS,
-          access_token: access_token
-        )
-        AddLabelToAnIssue.call!(
-          repo_full_name: repo_full_name,
-          id: id,
-          label: Constants::READY_FOR_REVIEW,
-          access_token: access_token
-        )
+        remove_label(id, Constants::IN_PROGRESS)
+        add_label_to_an_issue(id, Constants::READY_FOR_REVIEW)
       end
+    end
+
+    private
+
+    def remove_label(id, label)
+      RemoveLabel.call!(
+        repo_full_name: repo_full_name,
+        id: id,
+        label: label,
+        access_token: access_token
+      )
+    end
+
+    def add_label_to_an_issue(id, label)
+      AddLabelToAnIssue.call!(
+        repo_full_name: repo_full_name,
+        id: id,
+        label: label,
+        access_token: access_token
+      )
     end
   end
 end
