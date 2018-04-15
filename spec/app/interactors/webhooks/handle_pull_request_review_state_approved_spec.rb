c9d1164cd80e334e8a2985c81b9fdd817e5deb31
Pablo Cantero <pablohstc@gmail.com>
2018-04-14 22:57:49 -0400

    RSpec review approved

---
+++ spec/app/interactors/webhooks/handle_pull_request_review_state_approved_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+module Webhooks
+  RSpec.describe HandlePullRequestReviewStateApproved do
+    let(:payload) { { 'pull_request' => { 'body' => body } } }
+    let(:body) { 'Closes #123' }
+    let(:number) { 'number' }
+    let(:repo_full_name) { 'octocat/Hello-World' }
+    let(:access_token) { 'token' }
+
+    describe '#call' do
+      specify do
+        allow(Webhooks::FindFixableIssues).to receive(:call!).with(body: body).and_return(double(numbers: [number]))
+
+        expect(RemoveLabel).to receive(:call!).with(
+          repo_full_name: repo_full_name,
+          number: number,
+          label: [Constants::REVIEW_REQUESTED, Constants::REJECTED],
+          access_token: access_token
+        )
+
+        described_class.call!(payload: payload, repo_full_name: repo_full_name, access_token: access_token)
+      end
+    end
+  end
+end
