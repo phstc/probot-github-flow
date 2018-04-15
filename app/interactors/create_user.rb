ac1bc94b286800ce16623c26c6f03f23ee6aeb14
Pablo Cantero <pablohstc@gmail.com>
2018-04-15 18:52:57 -0400

    Create a user after login

--- app/interactors/create_user.rb
+++ app/interactors/create_user.rb
@@ -2,15 +2,35 @@ class CreateUser
   include Interactor
   include InteractorHelper
 
+  def_delegators :context, :client_id, :client_secret, :session_code
+
   def call
+    context.access_token = access_token
+
+    user_hash = client.user
+
+    user = User.where(login: user_hash[:login]).first_or_initialize
+    user.email = user_hash[:email]
+    user.company = user_hash[:company]
+    user.name = user_hash[:name]
+    user.access_token = access_token
+
+    user.save
+
+    context.user = user
+  end
+
+  private
+
+  def access_token
+    return context.access_token if context.access_token
+
     result = RestClient.post(
       'https://github.com/login/oauth/access_token',
-      { client_id: context.client_id, client_secret: context.client_secret, code: context.session_code },
+      { client_id: client_id, client_secret: client_secret, code: session_code },
       accept: :json
     )
 
     context.access_token = JSON.parse(result)['access_token']
-
-    context.user = client.user
   end
 end
