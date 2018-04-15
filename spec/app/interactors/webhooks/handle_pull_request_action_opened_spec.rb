25cd1ce3fb481d01bd77d63e9d2e2178009f1c4a
Pablo Cantero <pablohstc@gmail.com>
2018-04-15 09:47:21 -0400

    Add pending specs

---
+++ spec/app/interactors/webhooks/handle_pull_request_action_opened_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+module Webhooks
+  RSpec.describe HandlePullRequestActionOpened do
+    let(:payload) { { 'pull_request' => { 'body' => body } } }
+    let(:body) { 'Closes #123' }
+    let(:number) { 'number' }
+    let(:repo_full_name) { 'octocat/Hello-World' }
+    let(:access_token) { 'token' }
+
+    describe '#call' do
+      it 'test me'
+    end
+  end
+end
