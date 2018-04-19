ded3f2ea17b4cb4a8ea3b9af02dc8afa74885c57
Pablo Cantero <pablohstc@gmail.com>
2018-04-19 08:02:32 -0400

    Skip CSRF for webhook calls

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,6 @@
 class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
   def create
     Webhooks::HandleWebhook.call!(
       access_token: ACCESS_TOKEN,
