670d6c6fa499d806dbac8d69a35b9f93184ed9d0
Pablo Cantero <pablohstc@gmail.com>
2018-04-21 18:10:35 -0400

    WIP issues.closed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-module.exports = (robot) => {
-  // Your code here
+module.exports = robot => {
+  const READY_FOR_REVIEW = 'ready for review'
+  const REJECTED = 'rejected'
+  const REVIEW_REQUESTED = 'review requested'
+  const IN_PROGRESS = 'in progress'
+
   robot.log('Yay, the app was loaded!')
 
   // For more information on building apps:
@@ -7,4 +11,50 @@ module.exports = (robot) => {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
+  robot.on('issues.labeled', async context => {
+    robot.log(context)
+  })
+
+  const removeLabels = async (github, owner, repo, number, labels) => {
+    labels.forEach(async name => {
+      await github.issues.removeLabel({
+        owner,
+        repo,
+        number,
+        name
+      })
+    })
+  }
+
+  robot.on('issues.closed', async context => {
+    // robot.log(context)
+
+    await removeLabels(
+      context.github,
+      context.payload.repository.owner.login,
+      context.payload.repository.name,
+      context.payload.issue.number,
+      [IN_PROGRESS, READY_FOR_REVIEW, REVIEW_REQUESTED]
+    )
+  })
+
+  robot.on('pull_request.closed', async context => {
+    robot.log(context)
+  })
+
+  robot.on(
+    ['pull_request.opened', 'pull_request.edited', 'pull_request.reopened'],
+    async context => {
+      robot.log(context)
+    }
+  )
+
+  robot.on('pull_request.review_requested', async context => {
+    robot.log(context)
+  })
+
+  robot.on('pull_request_review', async context => {
+    robot.log(context)
+  })
 }
