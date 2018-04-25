f02ace6db9d5a7981afcbb9b72ba5a5ccf5a564c
Pablo Cantero <pablohstc@gmail.com>
2018-04-25 13:24:50 -0400

    Test handleIssuesLabeled

---
+++ test/handleIssuesLabeled.test.js
@@ -0,0 +1,71 @@
+const handleIssuesLabeled = require('../lib/handleIssuesLabeled')
+
+const {
+  IN_PROGRESS,
+  READY_FOR_REVIEW,
+  REVIEW_REQUESTED,
+  REJECTED
+} = require('../lib/constants')
+
+const owner = 'owner'
+const repo = 'repo'
+const issue = {
+  number: '5678'
+}
+
+const github = {
+  issues: {
+    get: jest.fn(),
+    edit: jest.fn()
+  }
+}
+
+jest.mock('../lib/issues', () => ({
+  removeLabels: jest.fn(),
+  addLabels: jest.fn()
+}))
+
+const { addLabels, removeLabels } = require('../lib/issues')
+
+beforeEach(() => {
+  removeLabels.mockReset()
+  addLabels.mockReset()
+})
+
+test('labeled with READY_FOR_REVIEW', async () => {
+  await handleIssuesLabeled(github, owner, repo, {
+    issue,
+    label: { name: READY_FOR_REVIEW }
+  })
+
+  expect(removeLabels).toBeCalledWith(github, owner, repo, issue.number, [
+    IN_PROGRESS
+  ])
+})
+
+test('labeled with REJECTED', async () => {
+  await handleIssuesLabeled(github, owner, repo, {
+    issue,
+    label: { name: REJECTED }
+  })
+
+  expect(removeLabels).toBeCalledWith(github, owner, repo, issue.number, [
+    IN_PROGRESS,
+    READY_FOR_REVIEW
+  ])
+})
+
+test('labeled with REVIEW_REQUESTED', async () => {
+  await handleIssuesLabeled(github, owner, repo, {
+    issue,
+    label: { name: REVIEW_REQUESTED }
+  })
+
+  expect(removeLabels).toBeCalledWith(github, owner, repo, issue.number, [
+    IN_PROGRESS
+  ])
+
+  expect(addLabels).toBeCalledWith(github, owner, repo, issue.number, [
+    READY_FOR_REVIEW
+  ])
+})
