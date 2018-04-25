78c41459a9ab7bab62760cb9b8f9ea35db1c1e5c
Pablo Cantero <pablohstc@gmail.com>
2018-04-25 10:59:45 -0400

    Extract out handlePullRequestReview

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const handlePullRequestClosed = require('./lib/handlePullRequestClosed')
 const handlePullRequestOpened = require('./lib/handlePullRequestOpened')
 const handleIssuesLabeled = require('./lib/handleIssuesLabeled')
 const handlePullRequestReviewRequested = require('./lib/handlePullRequestReviewRequested')
+const handlePullRequestReview = require('./lib/handlePullRequestReview')
 const {
   IN_PROGRESS,
   READY_FOR_REVIEW,
@@ -63,33 +64,12 @@ module.exports = robot => {
   })
 
   robot.on('pull_request_review', async context => {
-    switch (context.payload.review.state) {
-      case 'changes_requested':
-        await findFixableIssues(context.payload.pull_request.body).forEach(
-          async number => {
-            await addLabels(
-              context.github,
-              context.payload.repository.owner.login,
-              context.payload.repository.name,
-              number,
-              [REJECTED]
-            )
-          }
-        )
-        break
-      case 'approved':
-        await findFixableIssues(context.payload.pull_request.body).forEach(
-          async number => {
-            await removeLabels(
-              context.github,
-              context.payload.repository.owner.login,
-              context.payload.repository.name,
-              number,
-              [REVIEW_REQUESTED, REJECTED]
-            )
-          }
-        )
-        break
-    }
+    await handlePullRequestReview(
+      context.github,
+      context.payload.repository.owner.login,
+      context.payload.repository.name,
+      context.payload.pull_request,
+      context.payload.review
+    )
   })
 }
