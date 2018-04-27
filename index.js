5cbf1295690c47cef2c0f19f465059d80a7c4c81
Pablo Cantero <pablohstc@gmail.com>
2018-04-26 21:40:01 -0400

    Add rollbar

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const rollbar = require('rollbar')
 const handlePullRequestClosed = require('./lib/handlePullRequestClosed')
 const handlePullRequestOpened = require('./lib/handlePullRequestOpened')
 const handleIssuesLabeled = require('./lib/handleIssuesLabeled')
@@ -5,61 +6,95 @@ const handlePullRequestReviewRequested = require('./lib/handlePullRequestReviewR
 const handlePullRequestReview = require('./lib/handlePullRequestReview')
 const handleIssuesClosed = require('./lib/handleIssuesClosed')
 
+if (process.env.NODE_ENV === 'production') {
+  rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN)
+}
+
+const handleError = e => {
+  if (process.env.NODE_ENV === 'production') {
+    rollbar.handleError(e)
+  }
+}
+
 module.exports = robot => {
   robot.on('issues.labeled', async context => {
-    await handleIssuesLabeled(
-      context.github,
-      context.payload.repository.owner.login,
-      context.payload.repository.name,
-      context.payload
-    )
-  })
-
-  robot.on('issues.closed', async context => {
-    await handleIssuesClosed(
-      context.github,
-      context.payload.repository.owner.login,
-      context.payload.repository.name,
-      context.payload.issue
-    )
-  })
-
-  robot.on('pull_request.closed', async context => {
-    await handlePullRequestClosed(
-      context.github,
-      context.payload.repository.owner.login,
-      context.payload.repository.name,
-      context.payload
-    )
-  })
-
-  robot.on(
-    ['pull_request.opened', 'pull_request.edited', 'pull_request.reopened'],
-    async context => {
-      await handlePullRequestOpened(
+    try {
+      await handleIssuesLabeled(
         context.github,
         context.payload.repository.owner.login,
         context.payload.repository.name,
         context.payload
       )
+    } catch (e) {
+      handleError(e)
+    }
+  })
+
+  robot.on('issues.closed', async context => {
+    try {
+      await handleIssuesClosed(
+        context.github,
+        context.payload.repository.owner.login,
+        context.payload.repository.name,
+        context.payload.issue
+      )
+    } catch (e) {
+      handleError(e)
+    }
+  })
+
+  robot.on('pull_request.closed', async context => {
+    try {
+      await handlePullRequestClosed(
+        context.github,
+        context.payload.repository.owner.login,
+        context.payload.repository.name,
+        context.payload
+      )
+    } catch (e) {
+      handleError(e)
+    }
+  })
+
+  robot.on(
+    ['pull_request.opened', 'pull_request.edited', 'pull_request.reopened'],
+    async context => {
+      try {
+        await handlePullRequestOpened(
+          context.github,
+          context.payload.repository.owner.login,
+          context.payload.repository.name,
+          context.payload
+        )
+      } catch (e) {
+        handleError(e)
+      }
     }
   )
 
   robot.on('pull_request.review_requested', async context => {
-    await handlePullRequestReviewRequested(
-      context.github,
-      context.payload.repository.owner.login,
-      context.payload.repository.name,
-      context.payload
-    )
+    try {
+      await handlePullRequestReviewRequested(
+        context.github,
+        context.payload.repository.owner.login,
+        context.payload.repository.name,
+        context.payload
+      )
+    } catch (e) {
+      handleError(e)
+    }
   })
 
   robot.on('pull_request_review', async context => {
-    await handlePullRequestReview(
-      context.github,
-      context.payload.repository.owner.login,
-      context.payload.repository.name,
-      context.payload
-    )
+    try {
+      await handlePullRequestReview(
+        context.github,
+        context.payload.repository.owner.login,
+        context.payload.repository.name,
+        context.payload
+      )
+    } catch (e) {
+      handleError(e)
+    }
   })
 }
